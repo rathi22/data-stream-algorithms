60ec21f8a69cad022a63ea4bbfd6b0f22c2a83a3
rathi22 <rajatrathidgr81@gmail.com>
2018-07-15 23:50:34 +0530

    Update

--- final-sketch.cpp
+++ final-sketch.cpp
@@ -7,7 +7,10 @@
 	The unique property of a sketch is that the result
 	by the algorithm for a combination of two streams,
 	is a space-efficient combiniation of the results for
-	the 2 individual streams   
+	the 2 individual streams.
+	Final Sketch simply improves upon Count Sketch
+	by giving a epsilon-delta approximation using the 
+	Median Trick.   
 	*/
 #include <bits/stdc++.h>
 using namespace std;
@@ -15,6 +18,16 @@ using namespace std;
 // Structure for 2-universal hash function
 struct hashfn{
 	int N,K,LOG_N,POLY,a,b;
+
+	hashfn(){
+		N = 16384;
+		K = 2;
+		LOG_N = 14;
+		POLY = 17475;
+		a = rand()%N;
+		b = rand()%N;
+	}
+
 	hashfn(int n, int k, int logn, int poly){
 		N = n;
 		K = k;
@@ -58,22 +71,30 @@ int main(){
 	srand(time(NULL));
 	double EPS = 1;
 	int K = 3/(EPS*EPS);
-	int C[K] = {0};
-	hashfn h(16384,K,14,17475);
-	hashfn g(16384,2,14,17475);
+	int t = 100;	// Variable Parameter Depending upon delta
+	int C[t][K] = {0};
+	hashfn h[t];
+	hashfn g[t];
+	for(int i=0;i<t;i++)
+		h[i].K = K;
 
 	int m,j,c;	// Length of stream
 	cin>>m;
 	while(m--){
 		// Turnstile Model
 		cin>>j>>c;
-		C[h.hash(j)] += c*(2*g.hash(j)-1);
+		for(int i=0;i<t;i++)
+			C[i][h[i].hash(j)] += c*(2*g[i].hash(j)-1);
 	}
 
+	int v[t];
 	int q,a;	// Number of Queries
 	cin>>q;
 	while(q--){
 		cin>>a;
-		cout<<"Frequency="<< (2*g.hash(a)-1)*C[h.hash(a)]<<endl;
+		for(int i=0;i<t;i++)
+			v[i] = (2*g[i].hash(a)-1)*C[i][h[i].hash(a)];
+		sort(v,v+t);
+		cout<<"Frequency="<< v[t/2] <<endl;
 	}
 }
